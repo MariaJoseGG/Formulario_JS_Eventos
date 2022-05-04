945d7d400d29e93fcc443101868654959860a7e1
MariaJoseGG <mgiraldogu@unal.edu.co>
2022-05-04 17:09:45 -0500

    Se guarda todo el formulario en un solo archivo HTML y se agregan los nombres al README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Formulario_JS_Eventos
+##Integrantes:
+Tatiana Alexandra Betancurth Paiba
+Dayane Michelle Ceballos Cardona
+María José Giraldo Gutiérrez
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function desplegar() {
     if(document.getElementById("flexRadioDefault1").checked) {
         document.getElementById("lista").innerHTML=
@@ -18,9 +17,7 @@ function desplegar() {
         document.getElementById("lista").innerHTML=` `;
     }
 }
-=======
 
->>>>>>> f07b190d32f9b7cad2732f19764cd0e6508799db
 function apellido4(){
     let ape=document.getElementById("apellido").value;
     let sinEspacios=ape.replace(/ /g, "");
